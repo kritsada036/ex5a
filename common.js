272183a3705540b38a5b3fc6cb92cf8e08764cca
Cheep <pop.6573@gmail.com>
2023-02-16 11:23:23 +0700

    refactor#2 adding paceCal fnn

--- common.js
+++ common.js
@@ -1,7 +1,31 @@
+function paceValidate(pace){
+	//---------------------------
+	if(isNaN(pace)){
+		msg = "Please verify that the pace you have entered is correct!!!";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+	if(!(pace >= 3 && pace <= 10)){
+		msg = "Running pace must be between 3 and 10 min/km";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+}
+function paceCal(pace,dist){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return ("min":min,"hour":hour);
+}
+
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
+	//---------------------------
+	var chk = paceValidate(pace);
+	if(chk != null)return chk;
 	//---------------------------
 		if(isNaN(pace)){
 			msg = "Please verify that the pace you have entered is correct!!!";
@@ -12,12 +36,7 @@ function calculate(p,d){
 			return {"status":0,"message":msg,"hour":0,"min":0};
 		}
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
